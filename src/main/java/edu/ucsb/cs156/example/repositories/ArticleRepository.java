bbbd6bac24e90bf026696d4f7a4d56f4845bda75
Tuan Le <tuancle@ucsb.edu>
2025-04-22 17:05:10 -0700

    removed commented out code

--- src/main/java/edu/ucsb/cs156/example/repositories/ArticleRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/ArticleRepository.java
@@ -11,10 +11,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleRepository extends CrudRepository<Article, Long> {
-  /**
-   * This method returns all UCSBDate entities with a given quarterYYYYQ.
-   * @param quarterYYYYQ quarter in the format YYYYQ (e.g. 20241 for Winter 2024, 20242 for Spring 2024, 20243 for Summer 2024, 20244 for Fall 2024)
-   * @return all UCSBDate entities with a given quarterYYYYQ
-   */
-  // Iterable<Article> findAllByQuarterYYYYQ(String quarterYYYYQ);
+  
 }
